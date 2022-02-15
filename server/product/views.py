d76ac1c339507c921a5ca881d9ec624b25298fb5
Luis Daniel Cova <58636486+LuisDanielCova@users.noreply.github.com>
2022-02-15 11:42:01 -0400

    fix(server): fix product detail view

--- server/product/views.py
+++ server/product/views.py
@@ -17,14 +17,14 @@ class NewProductsList(APIView):
 class ProductDetail(APIView):
     def get_object(self, category_slug, product_slug):
         try:
-            return Product.objects.filter(category_slug=category_slug).get(
+            return Product.objects.filter(category__slug=category_slug).get(
                 slug=product_slug
             )
         except Product.DoesNotExist:
             raise Http404
 
     def get(self, request, category_slug, product_slug, format=None):
-        product = self.get_object(self, category_slug, product_slug)
+        product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
 
         return Response(serializer.data)
